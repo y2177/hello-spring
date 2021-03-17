c4fb173614e117bec3f53cc55ae98bae1b3ea603
FGD2020 <60589339+y2177@users.noreply.github.com>
2021-03-17 01:07:44 -0500

    Commit

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,50 +3,48 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
-@ResponseBody //if put ResponseBody here, don't need it above each method
-@RequestMapping("hello")
-//Can remove value = 'hello' and "hello/ portion of @GetMapping("hello/{name}")
 public class HelloController {
 
-    //@GetMapping("hello")
-    //@ResponseBody
-    ////It lives at root path /hello
-    //public String hello() {
-    //    return "Hello, Spring!";
-    //}
+    //    // Handle request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
-    //lives /hello/goodbye
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    //live /hello/hello
-    //Dynamically request of the form /hello?name=Launchcode with "Launchcode" being replaceable
-    //Name in url matches Queryparameter/method parameter. Will populate method parameter name
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
-    //Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
-    //Form has input called name (input type's name = 'name') that also matches String in hello method
-    //which also has that form action
-    //Inserting method = 'post' makes it a post request instead of default get request
-    //lives at hello/form
+
     @GetMapping("form")
-    public String helloForm(){
+    @ResponseBody
+    public String helloForm() {
         return "<html>" +
                 "<body>" +
                 "<form action = 'hello' method = 'post'>" + // submit a request to /hello
-                "<input type='text' name = 'name'>" +
-                "<input type = 'submit' value='Greet me!'>" +
+                "<input type = 'text' name = 'name' >" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
---
+++ src/main/resources/templates/form/html.kt
@@ -0,0 +1,5 @@
+package templates.form
+
+class html {
+
+}
